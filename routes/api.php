62c749797d82c4f4b5d70dd98334d547e8650ff1
mmxnu <manucehrdzuraev50@gmail.com>
2025-04-17 16:48:18 +0500

    second commit

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ResumeController;
 use \App\Http\Controllers\Api\ExperienceController;
 use App\Http\Controllers\Api\CandidateController;
-use Illuminate\Support\Facades\Route;
 
 Route::apiResource('application', \App\Models\Application::class);
 Route::apiResource('resume', \App\Models\Resume::class);
